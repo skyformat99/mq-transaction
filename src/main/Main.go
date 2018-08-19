62d4e2481abc3c908f5e2a6301565b2ae37dbf7a
zhuxiujia <zhuxiujia@qq.com>
2018-08-19 23:55:27 +0800

    初步提交

--- src/main/Main.go
+++ src/main/Main.go
@@ -1,5 +1,17 @@
 package main
 
+import (
+	"github.com/kataras/iris"
+)
+
 func main()  {
-	
+ startHttp()
 }
+func startHttp() {
+	app := iris.New()
+	app.Get("/", func(ctx iris.Context) {
+		ctx.JSON(iris.Map{"hello": "yes hello!"})
+	})
+	app.Run(iris.Addr("localhost:8080"), iris.WithoutServerError(iris.ErrServerClosed))
+}
+
